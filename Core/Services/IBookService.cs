1dd611c276e40858c31e02bd97e1be760f52b2a0
Filip Sîrboiu <141073490+FilipS14@users.noreply.github.com>
2024-05-27 23:14:04 +0300

    Add files via upload

---
+++ Core/Services/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Models;
+using DataBase.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<BookModel>> GetAllBooksAsync();
+        Task<BookModel> GetBookByIdAsync(int id);
+
+    }
+}

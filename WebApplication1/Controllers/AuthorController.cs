4702bb2678bba4aa9f0640490cb2adcdec2b95ac
Filip Sîrboiu <141073490+FilipS14@users.noreply.github.com>
2024-05-27 23:19:36 +0300

    Add files via upload

---
+++ WebApplication1/Controllers/AuthorController.cs
@@ -0,0 +1,26 @@
+﻿using Core.Models;
+using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers
+{
+    [Route("get-all")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IAuthorService _autorService;
+
+        public AuthorController(IAuthorService autorService)
+        {
+            _autorService = autorService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var autori = await _autorService.GetAllAuthorsAsync();
+            return Ok(autori);
+        }
+
+
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,34 @@
+﻿using DataBase.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Core.Services;
+using DataBase.Repositories;
+
+namespace WebApplication1
+{
+    public static class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddServices();
+            services.AddRepository();
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthorService,AuthorService>();
+            services.AddScoped<IBookService,BookService> ();
+        }
+
+        public static void AddRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IBookRepository,BookRepository>();
+            services.AddScoped<IAuthorRepository,AuthorRepository>();
+            services.AddDbContext<ProjectDbContext>();
+            services.AddScoped<DbContext, ProjectDbContext>();
+        }
+    }
+}
